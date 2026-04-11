2f44055b28eb09cc50d4d89097783c437c676e20
Mission-HERMES <Lucasrobineau@outlook.com>
2026-04-11 15:38:54 +0200

    Update ia-knowledge.js

--- ia-knowledge.js
+++ ia-knowledge.js
@@ -1,6 +1,6 @@
 function askBaseAI() {
-    const input = document.getElementById("ai-input").value.toLowerCase();
-    const chat = document.getElementById("ai-chat");
+    const input = document.getElementById("ai-input-row").value.toLowerCase();
+    const chat = document.getElementById("ai-output");
 
     const responses = [
         // MÉTÉO
